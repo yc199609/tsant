82da75aa2798bf0c73ca6bd5b2cc78794957432d
yc <3095220780@qq.com>
2019-08-12 17:11:25 +0800

    :sparkles: 头像的下拉菜单

--- src/components/container/container-header/index.tsx
+++ src/components/container/container-header/index.tsx
@@ -1,11 +1,32 @@
 import React, { useContext } from 'react'
-import { Layout, Icon, Avatar } from 'antd'
+import { Layout, Icon, Avatar, Menu, Dropdown } from 'antd'
 import { CollapsedContext } from '../context'
 const { Header } = Layout
 
 interface Iprops {
     setcollapsed:(arg:boolean) =>void
 }
+
+const menu = (
+    <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        2nd menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        3rd menu item
+      </a>
+    </Menu.Item>
+  </Menu>
+)
+
 
 const ContainerHeader:React.SFC<Iprops> = ({setcollapsed})=>{
     const collapsed = useContext(CollapsedContext)
@@ -16,7 +37,9 @@ const ContainerHeader:React.SFC<Iprops> = ({setcollapsed})=>{
                 type={collapsed ? 'menu-unfold' : 'menu-fold'}
                 onClick={()=>setcollapsed(!collapsed)}
             />
-            <Avatar className="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            <Dropdown overlay={menu}>
+                <Avatar className="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            </Dropdown>
         </Header>
     )
 }
