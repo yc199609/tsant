cabac1ff794089a8d259f0c788534cc0db1ac0eb
yc <3095220780@qq.com>
2019-08-17 22:15:26 +0800

    :construction: 编辑树组件

--- src/pages/employee/index.tsx
+++ src/pages/employee/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-// import { Editable } from 'components/editable'
+import { Tree, Button } from 'antd'
+// import { TreeProps } from 'antd/es/tree/Tree'
+import style from './style.module.scss'
 
-
-
+const { TreeNode } = Tree
 const Employee:React.SFC = () => {
     return (
-        <div>
-            员工
-            {/* <Editable rowKey={"code"}  /> */}
-        </div>
+        <Tree showLine defaultExpandedKeys={['0-0-0']} blockNode>
+            <TreeNode title="parent 1" key="0-0">
+            <TreeNode title= {
+                <div className={style.text}>
+                    <span>伟大的袁常</span>
+                    <Button type="link" onClick={()=>{console.log(23)}}>sdsds</Button>
+                </div>
+            } key="0-0-0">
+                <TreeNode title="leaf" key="0-0-0-0" />
+                <TreeNode title="leaf" key="0-0-0-1" />
+                <TreeNode title="leaf" key="0-0-0-2" />
+            </TreeNode>
+            <TreeNode title="parent 1-1" key="0-0-1">
+                <TreeNode title="leaf" key="0-0-1-0" />
+            </TreeNode>
+            <TreeNode title="parent 1-2" key="0-0-2">
+                <TreeNode title="leaf" key="0-0-2-0" />
+                <TreeNode title="leaf" key="0-0-2-1" />
+            </TreeNode>
+            </TreeNode>
+        </Tree>
     )
 }
 
